469c50ce0b8a4986927bed2114cbe11d4c8b3d8e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-24 13:57:00 +0300

    init

--- ChoosingTheRightCustomTimingFunctionsForYourSwiftUIAnimation/ContentView.swift
+++ ChoosingTheRightCustomTimingFunctionsForYourSwiftUIAnimation/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var rotateHexagon = false
+    @State var scaleCircle = false
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(scaleCircle ? .yellow : .orange)
+                .scaleEffect(scaleCircle ? 1 : 0)
+                .animation(Animation.easeInOut(duration: 1).repeatForever().delay(2))
+                .onAppear() {
+                    self.scaleCircle.toggle()
+            }
+            
+            Image("hexagon")
+            .resizable()
+                .aspectRatio(contentMode: .fit)
+                .rotationEffect(.degrees(rotateHexagon ? 0 : -60), anchor: .center)
+                .frame(width: 100, height: 100)
+                .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+                .onAppear() {
+                    self.rotateHexagon.toggle()
+            }
+        }
+        
+        // Text("Waiting for next activity to begin...")
     }
 }
 
